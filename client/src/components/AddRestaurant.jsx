f829a0364c92d479fe1555b5a2f96ed4598559f8
Eunice Lok <eunicelp511@gmail.com>
2020-07-17 14:46:59 +0800

    Edited form in AddRestaurant.jsx with semantic ui and tested the add functionality

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react"
-import { Button } from 'semantic-ui-react'
 import RestaurantFinder from "../apis/RestaurantFinder"
 import { useContext } from "react"
 import { RestaurantsContext } from "../context/RestaurantsContext"
+import { Form } from 'semantic-ui-react'
+
+
+const options = [
+    { key: 'c', text: 'Central', value: 'central' },
+    { key: 'n', text: 'North', value: 'north' },
+    { key: 's', text: 'South', value: 'south' },
+    { key: 'e', text: 'East', value: 'east' },
+    { key: 'w', text: 'West', value: 'west' },
+  ]
 
 const AddRestaurant = () => {
     const {addRestaurants} = useContext(RestaurantsContext)
@@ -29,50 +38,36 @@ const AddRestaurant = () => {
     }
 
     return (
-        <div className="mb-4">
-            <form action="">
-                <div className="form-row">
-                    <div className="col">
-                        <input 
-                        value={name} 
+        <Form>
+            <Form.Group widths='equal'>
+                <Form.Input 
+                    fluid label="name" 
+                    placeholder="Restaurant name"
+                    onChange={(e) => setName(e.target.value)} 
+                    type="text" 
+                />
+
+                <Form.Select
+                    fluid
+                    label='location'
+                    options={options}
+                    placeholder='Location'
+                />
+                <Form.Input 
+                        fluid label="cuisine" 
+                        placeholder="Cuisine"
                         onChange={(e) => setName(e.target.value)} 
                         type="text" 
-                        className="form-control" 
-                        placeholder="Restaurant name"/>
-                    </div>
-                    <div className="col">
-                        <select  
-                        value={location} 
-                        onChange={(e) => setLocation(e.target.value)} 
-                        className="custom-select my-1 mr-sm-2">
-                            <option disabled>Location</option>
-                            <option value="Central">Central</option>
-                            <option value="North">North</option>
-                            <option value="South">South</option>
-                            <option value="East">East</option>
-                            <option value="West">West</option>
-                        </select>
-                    </div>
-                    <div className="col">
-                        <input  
-                        value={cuisine} 
-                        onChange={(e) => setCuisine(e.target.value)} 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="Cuisine"/>
-                    </div>
-                    <div className="col">
-                        <input  
-                        value={address} 
-                        onChange={(e) => setAddress(e.target.value)} 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="Address"/>
-                    </div>
-                    <button onClick={handleSubmit} type="submit" class="ui button">Add</button>
-                </div>
-            </form>
-        </div>
+                />
+                <Form.Input 
+                    fluid label="address" 
+                    placeholder="Address"
+                    onChange={(e) => setName(e.target.value)} 
+                    type="text" 
+                />
+            </Form.Group>
+                <Form.Button onClick={handleSubmit} type="submit">Add</Form.Button>
+        </Form>
     )
 }
 
